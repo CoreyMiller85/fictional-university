de794df605ac0c96efe673c354a2b7fb0790e22d
Corey Miller <coreymiller454@gmail.com>
2021-03-26 01:11:01 -0400

    add custom page 'past-events'

--- wp-content/themes/fictional-university/front-page.php
+++ wp-content/themes/fictional-university/front-page.php
@@ -20,7 +20,7 @@ get_header();
       <?php
       $today = date('Ymd');
       $homepageEvents = new WP_Query(array(
-        'posts_per_page' => -1,
+        'posts_per_page' => 2,
         'post_type' => 'event',
         'meta_key' => 'event_date',
         'orderby' => 'meta_value_num',
@@ -38,7 +38,6 @@ get_header();
       while ($homepageEvents->have_posts()) {
         $homepageEvents->the_post();
       ?>
-        <?php echo get_field('event_date'); ?>
         <div class="event-summary">
           <a class="event-summary__date t-center" href="#">
             <span class="event-summary__month"><?php
@@ -91,11 +90,11 @@ get_header();
           </a>
           <div class="event-summary__content">
             <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title() ?></a></h5>
-            <p><?php if (has_excerpt()) {
+            <?php if (has_excerpt()) {
                   echo get_the_excerpt();
                 } else {
                   echo wp_trim_words(get_the_content(), 18);
-                } ?></p>
+                } ?>
             <a href="<?php the_permalink(); ?>" class="nu gray"> Read more</a></p>
           </div>
         </div>
